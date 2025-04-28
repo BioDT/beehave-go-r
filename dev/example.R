841bc2bb40e9573f9bd6496856f5546b1640f47e
Nithador <tomas.martinovic@vsb.cz>
2025-04-28 13:26:06 +0200

    update example

--- dev/example.R
+++ dev/example.R
@@ -1,48 +1,52 @@
 library(beehave.go.r)
 
-weather_vector <- matrix(
-  c(
-    rep(10, 30),
-    rep(0, 335)
-  ),
-  nrow = 1
+
+# Create a weather vector with mostly no foraging (0 hours)
+# but 90 random days with 15 hours of foraging time
+weather_vector <- rep(0, 365)
+collection_days <- 120
+weather_vector[sample(70:265, collection_days)] <- sample(
+  0:15,
+  collection_days,
+  replace = TRUE
 )
 
 # Source all R scripts
 experiment <- beehave_init(add_default = FALSE) |>
-  add_weather_file(
-    "dev/data/weather_402.txt"
-  ) |>
-  add_flower_patches_from_map(
-    landuse_map = "dev/data/preidl-etal-RSE-2020_land-cover-classification-germany-2016.tif",
-    lookup_table = "dev/data/NectarPollenLookUp.csv",
-    location = data.frame(lat = 48.2, lon = 7.8)
-  ) |>
+  # add_weather_file(
+  #   "dev/data/weather_402.txt"
+  # ) |>
+  # add_flower_patches_from_map(
+  #   landuse_map = "dev/data/preidl-etal-RSE-2020_land-cover-classification-germany-2016.tif",
+  #   lookup_table = "dev/data/NectarPollenLookUp.csv",
+  #   location = data.frame(lat = 48.2, lon = 7.8)
+  # ) |>
+  add_weather_vector(weather_vector) |>
   add_parameter(
     list(
-      # InitialPatches = list(
-      #   Patches = list(
-      #     list(
-      #       DistToColony = 1000,
-      #       ConstantPatch = list(
-      #         Nectar = 5,
-      #         Pollen = 1,
-      #         NectarConcentration = 1,
-      #         DetectionProbability = 0.5
-      #       )
-      #     ),
-      #     list(
-      #       DistToColony = 200,
-      #       SeasonalPatch = list(
-      #         MaxNectar = 20,
-      #         MaxPollen = 10,
-      #         NectarConcentration = 1.5,
-      #         DetectionProbability = 0.2,
-      #         SeasonShift = 20
-      #       )
-      #     )
-      #   )
-      # ),
+      InitialPatches = list(
+        Patches = list(
+          list(
+            DistToColony = 1000,
+            ConstantPatch = list(
+              Nectar = 5,
+              Pollen = 1,
+              NectarConcentration = 1,
+              DetectionProbability = 0.5
+            )
+          ),
+          list(
+            DistToColony = 200,
+            SeasonalPatch = list(
+              MaxNectar = 20,
+              MaxPollen = 10,
+              NectarConcentration = 1.5,
+              DetectionProbability = 0.2,
+              SeasonShift = 20
+            )
+          )
+        )
+      ),
       reporters = c(
         "worker_cohorts",
         "stores"
@@ -53,6 +57,7 @@ experiment <- beehave_init(add_default = FALSE) |>
 
 plot_flower_patches(experiment)
 print(experiment)
+
 test <- run_simulation(experiment)
 
 bcs_plot_series(test, group = "stores")
